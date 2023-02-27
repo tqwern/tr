8753d6bf4f409020b91d4a2ed5ba0d7da9acad8c
tqwern <114398693+tqwern@users.noreply.github.com>
2023-02-27 17:50:09 +0500

    Add files via upload

---
+++ 73_82.js
@@ -0,0 +1,119 @@
+"use strict";
+// 1
+// let test = 1;
+// if (test > 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// 2
+// let test = 1;
+// if (test < 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// 3
+// let test = 1;
+// if (test >= 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+//4
+// let test = 1;
+// if (test <= 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+
+
+// 74
+// let test = 1;
+// if (test == 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+
+
+// 75
+// let test = 1;
+// if (test != 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+
+
+// 76
+// 1
+// let test1 = 1;
+// let test2 = 2;
+// if (test2 > test1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 2
+// let test1 = 1;
+// let test2 = 2;
+// if (test2 == test1) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// 77
+// let test = 'abc';
+// if (test == 'abc') {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// 78
+// let test1 = '123';
+// let test2 = 123;
+// if (test1 == test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+// 81
+// 1
+// let num = 3;
+// if (num > 0 && num < 5) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 2
+// let num = 15;
+// if (num >= 10 && num <= 20) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 3
+// let num1 = 2;
+// let num2 = 3;
+// if (num1 <= 1 && num2 >= 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
